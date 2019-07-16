3886044f12af9f3032058c32640684afe09e0744
Lipien <adam@pawtel.pl>
2019-07-16 16:03:56 +0200

    Typy danych: zadanie 1.

--- src/typydanych/DataTypes.java
+++ src/typydanych/DataTypes.java
@@ -1,3 +1,5 @@
+package typydanych;
+
 public class DataTypes {
     public static void main(String[] args) {
         System.out.print("Liczba int: ");
---
+++ src/typydanych/Zadanie1.java
@@ -0,0 +1,18 @@
+package typydanych;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        System.out.print("-Cześć Jan, słyszałem, że masz ");
+        System.out.print(25);
+        System.out.print(" lat i uczysz się programowania. Czy to prawda?\n");
+        System.out.print("-");
+        System.out.println(true);
+        System.out.print("-Czy Twoje inicjały to ");
+        System.out.print('J');
+        System.out.print(" ");
+        System.out.print('K');
+        System.out.println('?');
+        System.out.print("-Tak, ");
+        System.out.println(true);
+    }
+}
