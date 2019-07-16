f7e62b7d1b95ec8245e5fbf19453a7532a73625e
Lipien <adam@pawtel.pl>
2019-07-16 17:10:27 +0200

    Variables: exercise Car.

---
+++ src/variables/ExerciseCar.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class ExerciseCar {
+    public static void main(String[] args) {
+        String brand = "Skoda";
+        String model = "Superb";
+        int productionYear = 2017;
+        double price = 200000;
+
+        System.out.println("Car specification:");
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + productionYear);
+        System.out.println("Price: " + price);
+    }
+}
