7214d35d9a5dc416bc0421617d5992ac4b883c93
Lipien <adam@pawtel.pl>
2019-07-19 20:14:00 +0200

    Struktury sterujące: switch.

--- src/javabeans/PointApplication.java
+++ src/javabeans/PointApplication.java
@@ -21,5 +21,33 @@ public class PointApplication {
 
         pointController.minusY(p);
         System.out.println("minusY: " + p.getX() + " " + p.getY());
+
+        final int a = 1;
+        final int b = 2;
+        final int c = 3;
+        final int d = 4;
+
+        int choice = 2;
+
+        switch (choice) {
+            case a:
+                pointController.addX(p);
+                System.out.println("addX: " + p.getX() + " " + p.getY());
+                break;
+            case b:
+                pointController.addY(p);
+                System.out.println("addY: " + p.getX() + " " + p.getY());
+                break;
+            case c:
+                pointController.minusX(p);
+                System.out.println("minusX: " + p.getX() + " " + p.getY());
+                break;
+            case d:
+                pointController.minusY(p);
+                System.out.println("minusY: " + p.getX() + " " + p.getY());
+                break;
+            default:
+                System.out.println("Podano błędną wartość");
+        }
     }
 }
