3fbef5000edd93b9f0188979739fb911f5951212
Lipien <adam@pawtel.pl>
2019-07-25 08:33:04 +0200

    Fields marked static: example of usge.

---
+++ src/loops/exercisesandtests/World.java
@@ -0,0 +1,20 @@
+package loops.exercisesandtests;
+
+public class World {
+    public static void main(String[] args) {
+        Human adam = new Human("Adam", 185.5);
+        Human eve = new Human("Ewa", 167.5);
+
+        double avgHeight = (adam.getHeight() + eve.getHeight()) / 2;
+        Human.avgHeight = avgHeight; //avgHeight is static in class Human; "new" not needed
+
+        System.out.println("Pierwsi ludzie na Ziemi: ");
+        System.out.println(adam.getName() + " " + adam.getHeight() + "cm");
+        System.out.println(eve.getName() + " " + eve.getHeight() + "cm");
+
+        System.out.println("Średni wzrost: ");
+        System.out.println(adam.avgHeight);
+        System.out.println(eve.avgHeight);
+        System.out.println(Human.avgHeight);
+    }
+}
