34bbc0007c80724d1ba35aa4a5fab1859e287f44
Lipien <adam@pawtel.pl>
2019-07-31 11:59:48 +0200

    Class Object: method equals.

---
+++ src/classobject/School.java
@@ -0,0 +1,14 @@
+package classobject;
+
+public class School {
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Jan Kowalski", 22);
+        Student student2 = new Student("Jan Kowalski", 22);
+        System.out.println("student1 == student2");
+        System.out.println(student1 == student2); //false
+        System.out.println("student1.equals(student2)");
+        boolean studentEquals = student1.equals(student2); //true
+        System.out.println(studentEquals);
+    }
+}
