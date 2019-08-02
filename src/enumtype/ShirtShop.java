141f2494aa9d8a938da3068125a16e38d3076e2c
Lipien <adam@pawtel.pl>
2019-08-02 12:56:44 +0200

    Enum: second iteration.

--- src/enumtype/ShirtShop.java
+++ src/enumtype/ShirtShop.java
@@ -11,5 +11,10 @@ public class ShirtShop {
         tshirt.setSize(Size.MEDIUM);
         System.out.println(tshirt.getSize());
         System.out.println("Rozmiar Tshirta: " + tshirt.getSize().getDescription());
+
+        Size[] values = Size.values();
+
+        for(int i = 0; i<values.length; i++)
+        System.out.println(values[i]);
     }
 }
