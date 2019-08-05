525bddb9b8a6c50feeb8a6f5327eebf3589a1cf1
Lipien <adam@pawtel.pl>
2019-08-05 10:36:39 +0200

    String creation examples.

---
+++ src/exercisesandtests/StringTest.java
@@ -0,0 +1,11 @@
+package exercisesandtests;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String s1 = new String("String one");
+        String s2 = "String two";
+        String s3 = "String " + "three";
+
+        System.out.println(s3);
+    }
+}
