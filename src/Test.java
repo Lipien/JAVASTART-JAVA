942d606d34b22ccee0300c1ca31c50246b196a93
Lipien <adam@pawtel.pl>
2019-08-09 08:18:21 +0200

    Try and catch: conditional loop.

--- src/Test.java
+++ src/Test.java
@@ -6,13 +6,17 @@ public class Test {
         Scanner sc = new Scanner(System.in);
         System.out.println("Podaj liczbę: ");
         int number = 0;
-        try {
-            number = sc.nextInt();
-            System.out.println("Wczytano poprawną liczbę");
-        } catch (InputMismatchException ex) {
-            System.err.println("Podana wartość nie jest liczbą całkowitą");
-            ex.printStackTrace();
-        }
+        boolean error = true;
+        do {
+            try {
+                number = sc.nextInt();
+                error = false;
+                System.out.println("Wczytano poprawną liczbę");
+            } catch (InputMismatchException e) {
+                System.out.println("Nie podałeś liczby całkowitej, spróbuj jeszcze raz: ");
+                sc.nextLine();
+            }
+        } while (error);
         System.out.println("Podałeś: " + number);
         sc.close();
     }
