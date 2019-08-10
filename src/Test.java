4bec623455ab84c1bd6ba4972cd0aa11e2af8a20
Lipien <adam@pawtel.pl>
2019-08-10 17:57:28 +0200

    Exceptions: multi-catch.

--- src/Test.java
+++ src/Test.java
@@ -4,20 +4,29 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Podaj liczbę: ");
-        int number = 0;
+        int[] numbers = new int[2];
         boolean error = true;
+
         do {
             try {
-                number = sc.nextInt();
+                System.out.println("Podaj 1 liczbę: ");
+                numbers[0] = sc.nextInt();
+                sc.nextLine();
+                System.out.println("Podaj 2 liczbę: ");
+                numbers[1] = sc.nextInt();
+                sc.nextLine();
+
+                System.out.println("Którą wartość wyświetlić (1 lub 2)? ");
+                System.out.println("Liczba:" + numbers[sc.nextInt() - 1]);
                 error = false;
-                System.out.println("Wczytano poprawną liczbę");
-            } catch (InputMismatchException e) {
+            } catch (InputMismatchException ex) {
                 System.out.println("Nie podałeś liczby całkowitej, spróbuj jeszcze raz: ");
+                sc.nextLine();
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                System.out.println("Miało być 1 lub 2, zacznijmy od nowa: ");
                 sc.nextLine();
             }
         } while (error);
-        System.out.println("Podałeś: " + number);
         sc.close();
     }
 }
