19df652f57700cf9202942ba0fb6acba3af9cc9a
Lipien <adam@pawtel.pl>
2019-08-25 15:47:38 +0200

    Inner class example: engine in car.

---
+++ src/innerclasses/CarTest.java
@@ -0,0 +1,9 @@
+package innerclasses;
+
+public class CarTest {
+    public static void main(String[] args) throws InterruptedException {
+        Car car = new Car();
+        car.refuel(40);
+        car.go();
+    }
+}
