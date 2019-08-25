4e302022e7d93a067ae417c93d74c89a33775fb7
Lipien <adam@pawtel.pl>
2019-08-25 14:05:11 +0200

    Interface: geomtry figures example.

---
+++ src/interfaces/Shape.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+interface Shape {
+
+    public static final double PI = 3.14;
+
+    // metody są domyślnie publiczne i abstrakcyjne
+    double calculateArea();
+
+    public double calculatePerimeter();
+}
