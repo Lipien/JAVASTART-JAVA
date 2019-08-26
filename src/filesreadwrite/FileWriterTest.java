03e15ec691665ffc5561aa0f7f29838dbf30b6eb
Lipien <adam@pawtel.pl>
2019-08-26 19:09:16 +0200

    FileWriter example.

---
+++ src/filesreadwrite/FileWriterTest.java
@@ -0,0 +1,23 @@
+package filesreadwrite;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterTest {
+    public static void main(String[] args) {
+        String fileName = "testFile.txt";
+        try (
+                var fileWriter = new FileWriter(fileName, true);
+                var writer = new BufferedWriter(fileWriter);
+        ) {
+            writer.write("Bolek");
+            writer.newLine();
+            writer.write("Lolek");
+            writer.newLine();
+            writer.write("Karolek");
+        } catch (IOException e) {
+            System.err.println("Nie udało się zapisać pliku " + fileName);
+        }
+    }
+}
