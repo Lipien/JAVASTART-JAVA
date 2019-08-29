b0f833a2ae4d4ea1370c734de35ede2061831480
Lipien <adam@pawtel.pl>
2019-08-29 11:12:17 +0200

    Big numbers: example.

---
+++ src/bignumbers/BigNumbers.java
@@ -0,0 +1,12 @@
+package bignumbers;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class BigNumbers {
+    public static void main(String[] args) {
+        BigInteger big = new BigInteger("12345678910123456789");
+        big = big.multiply(BigInteger.valueOf(123456789));
+        System.out.println(big);
+    }
+}
