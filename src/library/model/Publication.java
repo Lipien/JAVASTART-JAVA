34a004f522f185a9a9df9c840cce08f52b3715fd
Lipien <adam@pawtel.pl>
2019-08-30 18:45:07 +0200

    Library: comparator.

--- src/library/model/Publication.java
+++ src/library/model/Publication.java
@@ -3,7 +3,7 @@ package library.model;
 import java.io.Serializable;
 import java.util.Objects;
 
-public abstract class Publication  implements Serializable {
+public abstract class Publication implements Serializable, Comparable<Publication> {
 
     private String title;
     private String publisher;
@@ -15,7 +15,7 @@ public abstract class Publication  implements Serializable {
         this.year = year;
     }
 
-    int getYear() {
+    public int getYear() {
         return year;
     }
 
@@ -23,7 +23,7 @@ public abstract class Publication  implements Serializable {
         this.year = year;
     }
 
-    String getTitle() {
+    public String getTitle() {
         return title;
     }
 
@@ -59,5 +59,10 @@ public abstract class Publication  implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(title, publisher, year);
+    }
+
+    @Override
+    public int compareTo(Publication p) {
+        return title.compareToIgnoreCase(p.title);
     }
 }
