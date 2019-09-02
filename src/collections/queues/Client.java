efdff848b4c42bf95ce2a0a73d54027d103dcf48
Lipien <adam@pawtel.pl>
2019-09-02 11:44:46 +0200

    Queue: examples.

---
+++ src/collections/queues/Client.java
@@ -0,0 +1,22 @@
+package collections.queues;
+
+class Client {
+    private String nickname;
+
+    public Client(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Override
+    public String toString() {
+        return nickname;
+    }
+}
