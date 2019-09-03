bfd8038a3bdb743f4a8fcd472981bd2d50069a97
Lipien <adam@pawtel.pl>
2019-09-03 10:47:58 +0200

    Function: example one.

---
+++ src/lambdaexpressions/Functions.java
@@ -0,0 +1,14 @@
+package lambdaexpressions;
+
+import java.util.function.Function;
+
+public class Functions {
+    public static void main(String[] args) {
+        // funkcja przyjmuje String i zwraca String
+        Function<String, String> func = text -> text.toLowerCase().trim();
+        String original = "   WIELKI NAPIS   ";
+        // wywołujemy funkcję przekazując jej original jako argument
+        String lowerCaseTrim = func.apply(original);
+        System.out.println(lowerCaseTrim);
+    }
+}
