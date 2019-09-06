643c54794f71c1a5f0921a009bde67a082ee424b
Lipien <adam@pawtel.pl>
2019-09-06 13:01:46 +0200

    Method reference: names sort.

---
+++ src/methodreference/NamesSort.java
@@ -0,0 +1,13 @@
+package methodreference;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class NamesSort {
+    public static void main(String[] args) {
+        List<String> name = Arrays.asList("Adam", "Basia", "Tomek", "Natasza", "Sywia");
+
+        name.sort(String::compareToIgnoreCase);
+        name.forEach(System.out::println);
+    }
+}
