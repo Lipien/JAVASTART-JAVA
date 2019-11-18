77622805a30f370f0c3e586ef57712ec9f3c72b0
Lipien <adam@pawtel.pl>
2019-11-18 16:00:18 +0100

    Design pattern: builder (pizza).

---
+++ src/exercisesandtests/testafter16/Pizza.java
@@ -0,0 +1,30 @@
+package exercisesandtests.testafter16;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza {
+
+    private List<String> ingredients;
+
+    public static class PizzaBuilder {
+        private List<String> ingredients = new ArrayList<>();
+
+        public PizzaBuilder ingredient(String ingredient) {
+            ingredients.add(ingredient);
+            return this;
+        }
+
+        public Pizza build() {
+            return new Pizza(ingredients);
+        }
+    }
+
+    private Pizza(List<String> ingredients) {
+        this.ingredients = new ArrayList<>(ingredients);
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+}
